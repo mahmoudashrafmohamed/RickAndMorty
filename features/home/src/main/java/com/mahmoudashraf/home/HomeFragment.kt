f8a44917f59267ce23285abb226b9455f07e8d8d
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2022-09-19 19:38:22 +0200

    add view binding extensions in core module

--- features/home/src/main/java/com/mahmoudashraf/home/HomeFragment.kt
+++ features/home/src/main/java/com/mahmoudashraf/home/HomeFragment.kt
@@ -1,19 +1,17 @@
 package com.mahmoudashraf.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mahmoudashraf.core.viewbinding.viewBinding
+import com.mahmoudashraf.home.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_home, container, false)
+  private val binding by viewBinding(FragmentHomeBinding::bind)
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
   }
 }
