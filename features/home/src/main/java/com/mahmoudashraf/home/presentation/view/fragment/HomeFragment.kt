ddd3472f49c43830441bc824647a0afadf5c4590
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2022-09-23 18:24:23 +0200

    add home fragment UI logic and bind UI data

--- features/home/src/main/java/com/mahmoudashraf/home/presentation/view/fragment/HomeFragment.kt
+++ features/home/src/main/java/com/mahmoudashraf/home/presentation/view/fragment/HomeFragment.kt
@@ -1,21 +1,24 @@
 package com.mahmoudashraf.home.presentation.view.fragment
 
 import android.os.Bundle
-import android.os.Handler
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.mahmoudashraf.core.viewbinding.viewBinding
 import com.mahmoudashraf.home.R
 import com.mahmoudashraf.home.databinding.FragmentHomeBinding
 import com.mahmoudashraf.home.presentation.view.adapter.CharactersListAdapter
+import com.mahmoudashraf.home.presentation.viewmodel.HomeScreenState
 import com.mahmoudashraf.home.presentation.viewmodel.HomeViewModel
 import com.orcas.entities.home.Character
-import com.orcas.entities.home.Location
-import com.orcas.entities.home.Origin
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -26,26 +29,45 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
         // sets VeilRecyclerView's properties
         binding.veilRecyclerView.run {
             setVeilLayout(R.layout.item_character)
             setAdapter(adapter)
             setLayoutManager(GridLayoutManager(context, 2))
             addVeiledItems(15)
-            viewModel.call()
-            Handler().postDelayed({
-                adapter.list = listOf(
-                    Character(
-                        1, "kkkkkkk", "", "", "",
-                        "", Origin("", ""), Location("", ""), "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltbb6a2b45de4350a1/60dc218776a3de39ad178c00/b866411a7b6ee04a29e1b5fa02a9f12dc38bf061.jpg", listOf(), "", ""
-                    ),  Character(
-                        12, "ccccccccccccccccccccc", "", "", "",
-                        "", Origin("", ""), Location("", ""), "", listOf(), "", ""
-                    )
-                )
-
-                unVeil()
-            }, 2000)
+            observeScreenState()
+            viewModel.getCharacters()
         }
+        adapter.setItemClickListener {
+            Log.e("clicked", "" + it.name)
+        }
+    }
+
+    private fun observeScreenState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.characters.collect { state ->
+                    when (state) {
+                        is HomeScreenState.Loading -> binding.veilRecyclerView.veil()
+                        is HomeScreenState.Success -> handleSuccessState(state.characters)
+                        is HomeScreenState.Error -> Toast.makeText(
+                            context,
+                            state.msg,
+                            Toast.LENGTH_LONG
+                        ).show()
+                        else -> Unit
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleSuccessState(characters: List<Character>) {
+        binding.veilRecyclerView.unVeil()
+        adapter.list = characters
     }
 }
