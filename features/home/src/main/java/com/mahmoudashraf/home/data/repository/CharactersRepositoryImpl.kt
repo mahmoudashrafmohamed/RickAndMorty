6ae8dcd55f2f13fca62baa5b9d8ecd4721ba805c
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2022-09-30 04:16:29 +0200

    enhancement in CharactersRepositoryImpl

--- features/home/src/main/java/com/mahmoudashraf/home/data/repository/CharactersRepositoryImpl.kt
+++ features/home/src/main/java/com/mahmoudashraf/home/data/repository/CharactersRepositoryImpl.kt
@@ -16,20 +16,19 @@ class CharactersRepositoryImpl @Inject constructor(
     private val charactersLocalDataSource: CharactersLocalDataSource
 ) : CharactersRepository {
     override suspend fun getCharacters(page: Int) = flow {
-        val localCharacters = charactersLocalDataSource.getCharacters(page)
-        if (localCharacters.isEmpty()) {
-            val response = charactersRemoteDataSource.getCharacters(page)
-            val mappedToCharacterEntity = response.data.map { it.asCharacterEntity() }
-            val mappedToCharacterLocalEntity = response.data.map { it.asCharacterLocalEntity() }
-            charactersLocalDataSource.addCharacters(mappedToCharacterLocalEntity)
-            emit(mappedToCharacterEntity)
-        } else {
-            emit(localCharacters.map { it.asCharacterEntity() })
-        }
+        charactersLocalDataSource.getCharacters(page)
+            .takeIf { it.isNotEmpty() }
+            ?.let { characters -> emit(characters.map { it.asCharacterEntity() }) }
+            ?: run {
+                charactersRemoteDataSource.getCharacters(page).let { response ->
+                    charactersLocalDataSource.addCharacters(response.data.map { it.asCharacterLocalEntity() })
+                    emit(response.data.map { it.asCharacterEntity() })
+                }
+            }
     }.flowOn(Dispatchers.IO)
 }
 
 
-fun CharacterRemoteEntity.asCharacterEntity() = Character(id, name,image)
+fun CharacterRemoteEntity.asCharacterEntity() = Character(id, name, image)
 fun CharacterRemoteEntity.asCharacterLocalEntity() = CharacterLocalEntity(id, image, name)
 fun CharacterLocalEntity.asCharacterEntity() = Character(id, name, image)
