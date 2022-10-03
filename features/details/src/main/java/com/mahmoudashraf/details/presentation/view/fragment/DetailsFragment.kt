7023fc93fa449a74f3be059548d842b17675b273
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2022-10-03 16:36:19 +0200

    add character image to details fragment

--- features/details/src/main/java/com/mahmoudashraf/details/presentation/view/fragment/DetailsFragment.kt
+++ features/details/src/main/java/com/mahmoudashraf/details/presentation/view/fragment/DetailsFragment.kt
@@ -3,6 +3,7 @@ package com.mahmoudashraf.details.presentation.view.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.mahmoudashraf.core.constants.NavigationConstants
 import com.mahmoudashraf.core.viewbinding.viewBinding
 import com.mahmoudashraf.details.R
@@ -13,7 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
     private val binding by viewBinding(FragmentDetailsBinding::bind)
-    val character by lazy { arguments?.getParcelable<Character>(NavigationConstants.CHARACTER) }
+    private val character by lazy { arguments?.getParcelable<Character>(NavigationConstants.CHARACTER) }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
@@ -22,6 +23,7 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     private fun initView() {
         character?.let {
             binding.tvCharacterName.text = it.name
+            Glide.with(binding.imgCharacter).load(it.image).into(binding.imgCharacter)
         }
     }
 }
